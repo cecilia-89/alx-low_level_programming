8550b51e7e0ca80eda872806437ae484f10e5c93
cecilia-89 <catabong89@gmail.com>
2022-03-13 15:11:12 +0000

    added: advanced tasks-2

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int x;
+
+	int y;
+
+	int z;
+
+	x = 48;
+
+	while (x < 56)
+	{
+		y = 49;
+
+		while (y < 57)
+		{
+			z = y + 1;
+
+			while (z < 58)
+			{
+				putchar(x);
+				putchar(y);
+				putchar(z);
+
+				if (x != 55 || y != 56 || z != 57)
+				{
+					putchar(',');
+					putchar(' ');
+
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
+
