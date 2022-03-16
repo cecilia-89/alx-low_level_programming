43318bd8351ea3815b7e6a353aa683794deaeaa4
cecilia-89 <catabong89@gmail.com>
2022-03-16 15:00:57 +0000

    modified: changes

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -25,17 +25,3 @@ int _abs(int c)
 
 }
 
-int main(void)
-{
-	    int r;
-
-	        r = _abs(-1);
-		    printf("%d\n", r);
-		        r = _abs(0);
-			    printf("%d\n", r);
-			        r = _abs(1);
-				    printf("%d\n", r);
-				        r = _abs(-98);
-					    printf("%d\n", r);
-					        return (0);
-}
