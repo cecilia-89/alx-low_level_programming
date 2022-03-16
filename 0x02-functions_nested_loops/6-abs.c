211830c464817c7ca6a2db849e0e0947878fa085
cecilia-89 <catabong89@gmail.com>
2022-03-16 14:59:28 +0000

    modified: changes

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _abs - returns ansolute value
@@ -11,9 +12,9 @@
 
 int _abs(int c)
 {
-	if (c == -c)
+	if (c < 0)
 	{
-		c *= -1 ;
+		c *= -1;
 
 		return (c);
 	}
@@ -24,3 +25,17 @@ int _abs(int c)
 
 }
 
+int main(void)
+{
+	    int r;
+
+	        r = _abs(-1);
+		    printf("%d\n", r);
+		        r = _abs(0);
+			    printf("%d\n", r);
+			        r = _abs(1);
+				    printf("%d\n", r);
+				        r = _abs(-98);
+					    printf("%d\n", r);
+					        return (0);
+}
