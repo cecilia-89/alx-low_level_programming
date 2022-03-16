a4747a3cb84ec96f9a170ea854b37685b1d44a87
cecilia-89 <catabong89@gmail.com>
2022-03-16 14:01:00 +0000

    added: task 7

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _abs - returns ansolute value
+ *
+ * @c: interger
+ *
+ * Return: always positive
+ *
+ */
+
+int _abs(int c)
+{
+	if (c == -c)
+	{
+		return (c);
+	}
+	else if (c == c)
+	{
+		return (c);
+	}
+
+}
+
