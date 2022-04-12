16a859799bc4059101a04a8379dcaa46557a574b
cecilia-89 <catabong89@gmail.com>
2022-04-12 04:18:35 +0000

    added: task 3

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,19 @@
+/**
+ * void init_dog - initializes a structure
+ *
+ * @d: base address
+ * @name: character
+ * @age: float
+ * @owner: character
+ */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	*d = 
+	dog.name = *name;
+	
+	dog.age = age;
+
+	dog.owner = *owner;	
 
 }
 
