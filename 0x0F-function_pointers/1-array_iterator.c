367f86f2e220973b2dbb33fc9426b4b1f496fb53
cecilia-89 <catabong89@gmail.com>
2022-04-13 11:13:37 +0000

    initial commit

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+/**
+ * array_iterator - prints a name
+ *
+ * @array: pointer to int
+ * @action: pointer to function
+ * @size_t: size of array
+ *
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	for (i = 0; i < size; i++)
+	{
+		(action)(array[i]);
+
+	}
+
+	printf("\n");
+
+}
+
+
