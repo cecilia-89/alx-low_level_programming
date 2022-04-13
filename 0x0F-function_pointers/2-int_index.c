5b3de2d0539aae8e9693a1adea567d275d1b79a1
cecilia-89 <catabong89@gmail.com>
2022-04-13 19:20:16 +0000

    added tasks

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,35 @@
+/**
+ * int_index - returns index if element matches
+ *
+ * @array: pointer to an array of int type
+ * @cmp: pointer to function
+ * @size: size of array
+ *
+ * Return: either -1 or index
+ *
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (size <= 0)
+		return (-1);
+
+	else
+	{
+		int i;
+
+		for (i = 0; i < size; i++)
+		{
+			int value;
+
+			value = (cmp)(array[i]);
+
+			if (value)
+				return (i);
+		}
+
+		return (-1);
+
+	}
+
+}
