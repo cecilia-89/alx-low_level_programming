9ea3a21409dd8ccb4f21ccaa71781a33600adffa
cecilia-89 <catabong89@gmail.com>
2022-04-14 03:59:42 +0000

    included: function_pointers.h

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,5 @@
+#include "function_pointers.h"
+
 /**
  * print_name - prints a name
  *
@@ -8,9 +10,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f = NULL)
-	{
+	if (f != NULL && name != NULL)
 		(f)(name);
-	}
 
 }
