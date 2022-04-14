e4a5388c917a494244f18a8370c23f5b46121aef
cecilia-89 <catabong89@gmail.com>
2022-04-14 04:29:11 +0000

    corrected: task 2

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,6 @@
-#include "function_pointers.h"
 #include <stdlib.h>
+#include "function_pointers.h"
+
 
 /**
  * array_iterator - prints a name
@@ -14,14 +15,16 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (!action || !array)
-		return;
-
-	for (i = 0; i < size; i++)
+	if (action && array)
 	{
-		(action)(array[i]);
 
-	}	
+		for (i = 0; i < size; i++)
+		{
+			(action)(array[i]);
+
+		}
+
+	}		
 
 
 
