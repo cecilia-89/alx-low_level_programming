298c5188b4625e5fb1d9448c68e1ef293b3982a7
cecilia-89 <catabong89@gmail.com>
2022-04-14 12:39:09 +0000

    minor changes

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -21,15 +21,15 @@ int sum_them_all(const unsigned int n, ...)
 	}
 
 
-	int i, sum;
+	unsigned int i;
 
-	sum = i = 0;
+	int sum = 0;
 
 	va_list ptr;
 
 	va_start(ptr, n);
 
-	for (; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(ptr, int);
 
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -22,7 +22,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(ptr, n);
 
-	for (int i = 0; i < n; i++)
+	for (unsigned int i = 0; i < n; i++)
 	{
 		if (separator && i != n - 1)
 			printf("%d%s", va_arg(ptr, int), separator);
