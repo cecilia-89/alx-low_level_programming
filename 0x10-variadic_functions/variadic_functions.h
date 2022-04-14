41e824b7dcc6f48de323570c05ff0a097818dd92
cecilia-89 <catabong89@gmail.com>
2022-04-14 12:33:14 +0000

    minor corrections

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,8 @@
 #ifndef FUNC_PROTO
 #define FUNC_PROTO
 
+#include <stddef.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
