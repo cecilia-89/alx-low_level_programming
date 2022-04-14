ff9fe32d165091714d8a3153f9b3875452cc0ed0
cecilia-89 <catabong89@gmail.com>
2022-04-14 18:39:53 +0000

    fixed: var declaration

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,16 +14,14 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
+	unsigned int i;
+
+	int sum = 0;
 
 	if (n == 0)
 	{
 		return (0);
 	}
-
-
-	unsigned int i;
-
-	int sum = 0;
 
 	va_list ptr;
 
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,11 +16,13 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	unsigned int i;
+
 	va_list ptr;
 
 	va_start(ptr, n);
 
-	for (unsigned int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		if (separator && i != n - 1)
 			printf("%d%s", va_arg(ptr, int), separator);
