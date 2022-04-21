bf3634eeebfbd2ea43967221f25fdaaa2374d28f
Cecilia Atabong <catabong89@gmail.com>
2022-04-22 00:46:34 +0100

    modified: if statement

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -24,10 +24,10 @@ size_t print_list(const list_t *h){
         {
             printf("[%d] %s", h -> len, h -> str);
 
-            h = h -> next;
-
         }
 
+        h = h -> next;
+        
         count++;
     }
 
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -14,10 +14,12 @@ size_t list_len(const list_t *h){
 
     int count = 0;
 
-    while (h -> next != 0){
-
+    while (h != NULL)
+    {
+        h = h -> next;
         count++;
     }
 
-   return (count);
+    return (count);
+    
 }
