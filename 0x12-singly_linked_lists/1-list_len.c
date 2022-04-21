6fa7d78d776b3a5204b1aacbfe144df75940a123
Cecilia Atabong <catabong89@gmail.com>
2022-04-22 00:56:55 +0100

    changed: data type

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,16 +3,16 @@
 /**
  * list_len - calculates the num of nodes
  * 
- * @h: single linked list
+ * @h:single linked list
  * 
- * Return: count of linked lists
+ * Return:count of linked lists
  * 
  */
 
 
 size_t list_len(const list_t *h){
 
-    int count = 0;
+    size_t count = 0;
 
     while (h != NULL)
     {
