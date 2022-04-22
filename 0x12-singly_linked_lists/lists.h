3ce849f51c6db4ac085f171deebd877683a71562
Cecilia Atabong <catabong89@gmail.com>
2022-04-22 01:46:31 +0100

    added: tasks

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -12,7 +12,7 @@ typedef struct list_s
  
 size_t print_list(const list_t *h);
 void free_list(list_t *head);
-list_t *add_node(list_t **head, const char *str);
+size_t list_len(const list_t *h);
 
 int _putchar(char c);
 
