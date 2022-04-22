5c22eab1841597fed388dcaec49ddb28fb4792e1
Cecilia Atabong <catabong89@gmail.com>
2022-04-22 01:04:04 +0100

    added: new line

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,18 +11,20 @@
 
 size_t print_list(const list_t *h){
 
-    size_t count = 0;
+    size_t count;
+
+    count = 0;
 
     while (h != NULL){
 
         if (h -> str == NULL){
 
-            printf("[%d] %s", 0, "(nil)");
+            printf("[%d] %s\n", 0, "(nil)");
         }
 
         else
         {
-            printf("[%d] %s", h -> len, h -> str);
+            printf("[%d] %s\n", h -> len, h -> str);
 
         }
 
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -12,7 +12,9 @@
 
 size_t list_len(const list_t *h){
 
-    size_t count = 0;
+    size_t count;
+
+    count = 0;
 
     while (h != NULL)
     {
