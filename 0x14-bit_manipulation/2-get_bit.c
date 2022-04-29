a091f62164284726b5e38d4b06873c7b67b17adf
Cecilia Atabong <catabong89@gmail.com>
2022-04-29 04:54:19 +0100

    second commit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,14 +13,18 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int bit;
+	unsigned int i;
 
-	bit = n >> index;
+	if (n == 0 && index < 64)
+		return (0);
 
-	if (bit > 32)
-		return (-1);
+	for (i = 0; i <= 63; n >>= 1, i++)
+	{
+		if (index == i)
+		{
+			return (n & 1);
+		}
+	}
 
-	return (bit & 1);
-
-
+	return (-1);
 }
