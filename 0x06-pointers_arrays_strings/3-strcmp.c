8e8de408669702ce027fc241183142e935a64473
cecilia-89 <catabong89@gmail.com>
2022-05-26 12:13:45 +0000

    corrected code

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -18,9 +18,9 @@ int _strcmp(char *s1, char *s2)
 		{
 			if (s1[i] > s2[i])
 
-				return (s1[0] - s2[0]);
+				return (s1[i] - s2[i]);
 
-			return (s2[0] - s1[0]);
+			return (s2[i] - s1[i]);
 		}
 	}
 
