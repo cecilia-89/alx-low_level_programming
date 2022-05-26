b719005a294682a28f23bb677d83c73cce1ec8a3
cecilia-89 <catabong89@gmail.com>
2022-05-26 12:45:49 +0000

    corrected code

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -23,7 +23,7 @@ char *cap_string(char *str)
 	{
 		for (i = 0; i < slen; i++)
 		{
-			if (str[count] > 96 && str[count] < 123 || str[count - 1] == s[i])
+			if ((str[count] > 96 && str[count] < 123) || str[count - 1] == s[i])
 			{
 				str[count]  = str[count] - 32;
 			}
