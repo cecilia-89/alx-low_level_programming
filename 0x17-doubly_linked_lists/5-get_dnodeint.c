7bb15dc6b88d53dce11b340ad46298ad61c726f2
cecilia-89 <catabong89@gmail.com>
2022-06-11 21:20:33 +0000

    Bug fixed

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,4 +1,12 @@
 #include "lists.h"
+#include "1-dlistint_len.c"
+
+/**
+ * *get_dnodeint_at_index - gets a node at a certain index
+ * @head:base node
+ * @index: the element to get
+ * Return: returns the element
+ */
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
@@ -6,11 +14,14 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	unsigned int count;
 
+	if (head == NULL)
+		return(NULL);
+
 	temp = head;
 
 	for (count = 0; count < index; count++)
 		temp = temp->next;
-	
+
 	return (temp);
 
 }
