802e274f00a72720c7c400cd78c9cf1e55cb41b9
cecilia-89 <catabong89@gmail.com>
2022-06-13 03:31:57 +0000

    last tasks

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -16,7 +16,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	temp = *head;
 
 	if (*head == NULL)
-		return (-1);
+		return (1);
 
 	if (index == 0)
 	{
--- 0x17-doubly_linked_lists/8-main.c
+++ 0x17-doubly_linked_lists/8-main.c
@@ -12,7 +12,7 @@ int main(void)
 	        head = NULL;
 		    add_dnodeint_end(&head, 0);
 		        add_dnodeint_end(&head, 1);
-			    add_dnodeint_end(&head, 2);
+			    add_dnodeint_end(&head, -2);
 			        add_dnodeint_end(&head, 3);
 				    add_dnodeint_end(&head, 4);
 				        add_dnodeint_end(&head, 98);
