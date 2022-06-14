55c92ce645c01fbb38e7a741589031a749817003
cecilia-89 <catabong89@gmail.com>
2022-06-14 03:46:40 +0000

    fourth commit

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-wget https://github.com/svelezg/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/inject.so -P ../
-export LD_PRELOAD=inject.so
+export LD_PRELOAD=../101-make_me_win.sh
