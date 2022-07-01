1633c2319253b5beda023fb663975234596e1cfc
cecilia-89 <catabong89@gmail.com>
2022-07-01 01:29:57 +0000

    initial commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,11 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ * Return: returns pointer to hash table
+ */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *h;
@@ -7,6 +13,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_node_t *hash_node;
 
 	h = malloc(sizeof(hash_table_t));
+
+	hash_node = malloc(sizeof(size));
 
 	h->size = size;
 
