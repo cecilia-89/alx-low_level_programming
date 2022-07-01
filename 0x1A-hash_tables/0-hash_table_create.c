9011239750f481113c2d614a990d7ef706baabd7
cecilia-89 <catabong89@gmail.com>
2022-07-01 01:39:41 +0000

    initial commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,6 +19,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hash_node = malloc(sizeof(size));
 
+	if (hash_node == NULL)
+		return (NULL)
+
 	h->size = size;
 
 	h->array = &hash_node;
