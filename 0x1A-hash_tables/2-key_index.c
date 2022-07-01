9b6fdaf7ce3c30a04d249b3849c2f7448d1b6676
cecilia-89 <catabong89@gmail.com>
2022-07-01 03:08:12 +0000

    refactored to betty prototype

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,5 +1,12 @@
 #include "hash_tables.h"
 
+/**
+ * key_index - computes the index of a key
+ * @key: index to find
+ * @size: size of the array
+ * Return: the index where @key is to be stored
+ */
+
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int djb_num;
