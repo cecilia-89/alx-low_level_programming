d32cb38e665ba586d9b3946accb3492e721fdd22
cecilia-89 <catabong89@gmail.com>
2022-10-06 21:55:40 +0100

    added tasks

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -10,7 +10,7 @@
 
 int jump_search(int *array, size_t size, int value)
 {
-	size_t low, step, i;
+	size_t low, step, temp, i;
 
 	if (array == NULL)
 		return (-1);
@@ -18,28 +18,38 @@ int jump_search(int *array, size_t size, int value)
 	low = 0;
 	step = sqrt(size);
 
-	while (low <= size)
+	while (low < size)
 	{
 		printf("Value checked array[%ld] = [%d]\n", low, array[low]);
 
-		if (array[step] >= value || step > size)
+		if (array[step] >= value)
 		{
-			printf("Value found between indexes [%ld] and [%ld]\n", low, step);
-
-			for (i = low; i <= step; i++)
+			temp = step;
+			while (temp != low)
 			{
-				if (i == size)
-					return (-1);
+				if (array[temp] == value || step >= size)
+				{
+					printf("Value found between indexes [%ld] and [%ld]\n", low, step);
+					for (i = low; i <= step; i++)
+					{
+						if (i == size)
+							break;
 
-				printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-				if (array[i] == value)
-					return (i);
+						printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+						if (array[i] == value)
+							return (i);
+					}
+							break;
+				}
+
+				else
+					temp--;
 			}
+
 		}
 
 		low = step;
 		step += sqrt(size);
-
 	}
 
 	return (-1);
--- 0x1E-search_algorithms/main.c
+++ 0x1E-search_algorithms/main.c
@@ -8,12 +8,12 @@
 int main(void)
 {
     int array[] = {
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 23, 44, 56, 999
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9
     };
     size_t size = sizeof(array) / sizeof(array[0]);
 
     printf("Found %d at index: %d\n\n", 6, jump_search(array, size, 6));
     printf("Found %d at index: %d\n\n", 1, jump_search(array, size, 1));
-    printf("Found %d at index: %d\n", 999, jump_search(array, size, 999));
+    printf("Found %d at index: %d\n", 18, jump_search(array, size, 18));
     return (EXIT_SUCCESS);
 }
